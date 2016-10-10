d1187db9844ef8a8255b1b3c4a1357916d5e5547
Amarendra Kumar <amar.arawat@gmail.com>
2016-10-10 16:45:02 +0530

    Change for Client App

--- client/src/main/java/com/home/ClientApplication.java
+++ client/src/main/java/com/home/ClientApplication.java
@@ -1,6 +1,8 @@
 package com.home;
 
 import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
 import com.hazelcast.core.HazelcastInstance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,10 +17,16 @@ public class ClientApplication {
 		SpringApplication.run(ClientApplication.class, args);
 	}
 
-	@Bean(name = "ClientInstance", destroyMethod = "shutdown")
-	public HazelcastInstance clientInstance() throws Exception{
-		return HazelcastClient.newHazelcastClient();
-	}
+    @Bean
+	public ClientConfig clientConfig(){
+        ClientConfig clientConfig = new ClientConfig();
+        ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+        networkConfig.setConnectionAttemptLimit(0);
+        return clientConfig;
+    }
 
-	
+    @Bean(name = "ClientInstance", destroyMethod = "shutdown")
+    public HazelcastInstance clientInstance() throws Exception{
+        return HazelcastClient.newHazelcastClient();
+    }
 }
